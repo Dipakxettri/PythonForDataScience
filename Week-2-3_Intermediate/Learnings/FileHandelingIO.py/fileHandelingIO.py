1fbb8de22860e67e88fbafc3c2ad5258357f50ad
Dipakxettri <codeepakghimire@gmail.com>
2025-09-19 12:29:19 +0545

    Done for now

---
+++ Week-2-3_Intermediate/Learnings/FileHandelingIO.py/fileHandelingIO.py
@@ -0,0 +1,20 @@
+# --------  What is File I/O? -----------
+# File I/O stands for File Input/Output.
+# Input (I) → Reading data from a file.
+# Output (O) → Writing data to a file.
+
+
+# ------- Open and Close a File: -------------
+# ---- Write: ----
+# file = open("Week-2-3_Intermediate/Learnings/FileHandelingIO.py/test.txt", "r") # Open for writing
+# file.write("Hello, world!")
+# file.close() # Always close the file
+
+# ---- read: ----
+# file2 = open("Week-2-3_Intermediate/Learnings/FileHandelingIO.py/test.txt", "r")
+# print(file2.read())  # Reads everything
+# file2.close()
+
+
+
+
