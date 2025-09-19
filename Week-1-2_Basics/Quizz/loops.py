16dfb10f9549ac93648168c253d3c9ebfc1b0384
Dipakxettri <codeepakghimire@gmail.com>
2025-09-19 08:53:17 +0545

    Updated

---
+++ Week-1-2_Basics/Quizz/loops.py
@@ -0,0 +1,28 @@
+# ---- Quiz(Loop Related): ----
+
+# ---- Q1. What is the output of the following code? ----
+# ```python
+# for i in range(2, 10, 2):
+#     print(i, end=" ")
+# ```
+# a) 2 4 6 8
+# b) 2 3 4 5 6 7 8 9
+# c) 2 4 6
+# d) Error
+ 
+# Ans = a
+
+
+# ---- Q2. Write a loop to print all even numbers between 1 and 50: ----
+
+# Ans :
+# for i in range(2,51,2):
+#     print(i)
+
+
+# ---- Q3. What is the difference between break and continue in loops?: ----
+
+# Ans:
+# break exits the loop entirely 
+# however, continue skips the current iteration and continue with next iteration
+
